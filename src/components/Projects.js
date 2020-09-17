42610dba4ad564099a356e33369a9dffea6c6272
Eduardo Lopez <eduardo@justadm.com>
2020-09-17 13:45:51 -0400

    added assets

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
 import styled from 'styled-components'
+import PageShield from '../assets/pageshield.jpeg'
+import Hercules from '../assets/hercules.jpeg'
 import ReactVisibilitySensor from 'react-visibility-sensor'
 
 export default function Projects(props) {
@@ -12,14 +14,38 @@ export default function Projects(props) {
                 <ProjectContainer>
                     <ReactVisibilitySensor partialVisibility onChange={isVisible => toggleProjects(isVisible)}>
                         <ProjectBox showBox={showProjects} translate={-15}>
-                            <ProjectCard>d</ProjectCard>
-                            <ProjectCard>d</ProjectCard>
+                            <ProjectCard backgroundImg={PageShield}>
+                                <span>
+                                    <p> PageShield 
+                                        <i className="material-icons">info</i>
+                                    </p>
+                                </span>
+                            </ProjectCard>
+                            <ProjectCard backgroundImg={Hercules}>
+                                <span>
+                                    <p> Hercules
+                                        <i className="material-icons">info</i>    
+                                    </p>
+                                </span>
+                            </ProjectCard>
                         </ProjectBox>
                     </ReactVisibilitySensor>
                     <ReactVisibilitySensor partialVisibility onChange={isVisible => toggleOthers(isVisible)}>
-                        <ProjectBox showBox={showOtherProjects} translate={15}>
-                            <ProjectCard>d</ProjectCard>
-                            <ProjectCard>d</ProjectCard>
+                    <ProjectBox showBox={showProjects} translate={15}>
+                            <ProjectCard backgroundImg={PageShield}>
+                                <span>
+                                    <p> PageShield 
+                                        <i className="material-icons">info</i>
+                                    </p>
+                                </span>
+                            </ProjectCard>
+                            <ProjectCard backgroundImg={Hercules}>
+                                <span>
+                                    <p> Hercules
+                                        <i className="material-icons">info</i>    
+                                    </p>
+                                </span>
+                            </ProjectCard>
                         </ProjectBox>
                     </ReactVisibilitySensor>
                 </ProjectContainer>
@@ -69,11 +95,45 @@ const ProjectBox = styled.div`
 `
 const ProjectCard = styled.div`
     display: flex;
-    background: white;
     margin: 0 15px;
     flex: 1;
     height: 100%;
-`
+    background-image: url(${props => props.backgroundImg});
+    background-size: cover;
+    display: flex;
+    justify-content: flex-end;
+    flex-direction: column;
+    & > span {
+        background-image: linear-gradient(rgb(0, 0, 0, 0), rgb(0, 0, 0, 1));
+        background-color: transparent;
+        height: 30%;
+        color: white;
+        display: flex;
+        justify-content: space-around;
+        font-size: 1.2rem;
+        transition: background 500ms, height 500ms, font-size 500ms;
+        flex-direction: column;
+        & > p {
+            display: flex;
+            align-items: center;
+            margin-left: 25px;
+        }
+        & > p > i {
+            padding-left: 10px;
+            opacity: 0;
+            transition: opacity 300ms;
+        }
+    }
+    &:hover > span {
+        background-color: rgb(0, 0, 0, 0.5);
+        height: 100%;
+        cursor: pointer;
+        & > p > i {
+            opacity: 1;
+        }
+
+    }
+    `
 const ProjectContainer = styled.div`
     height: 600px;
     display: grid;
