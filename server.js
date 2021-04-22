1be87b5ab4c910598e37abc239bbae7be329c7c1
Eddie Lopez <ejlopez00@gmail.com>
2021-04-22 12:58:14 -0400

    fixing server issues

--- server.js
+++ server.js
@@ -2,22 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use((req,res,next) => {
-    const host = req.get('host');
-    let redirectHost = host.includes('www') ? host : `www.${host}`;
+// app.use((req,res,next) => {
+//     const host = req.get('host');
+//     let redirectHost = host.includes('www') ? host : `www.${host}`;
 
-    console.log(redirectHost)
+//     console.log(redirectHost)
 
-    if (host.includes('localhost') || host.includes('eportfolio')) next();
-    next();
-    res.redirect('https://' + host + req.url);
-})
+//     if (host.includes('localhost') || host.includes('eportfolio')) next();
+//     next();
+//     res.redirect('https://' + host + req.url);
+// })
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-}).use(express.static(path.join(__dirname,'client','build')))
+})
 
 app.listen(process.env.PORT || 4200);
 console.log(`Listening on ${process.env.PORT || 4200}`)
