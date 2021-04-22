4e6cc8ad96f00317082a91136c1c667a1ba05fc1
Eddie Lopez <ejlopez00@gmail.com>
2021-04-22 12:25:58 -0400

    continuing the debug process

--- server.js
+++ server.js
@@ -4,19 +4,22 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-// app.use((req,res,next) => {
-//     const host = 'https://' + req.get('host');
+app.use((req,res,next) => {
+    const host = req.get('host');
+    let redirectHost = host.includes('www') ? host : `www.${host}`;
 
-//     if (host.includes('localhost') || host.includes('eportfolio')) next();
-//     // else if ((req.get('X-Forwarded-Proto') !== 'https')) {
-//     //     res.redirect(host + req.url);
-//     // } else next();
-// })
+    console.log(redirectHost)
+
+    if (host.includes('localhost')) next();
+    else if (!host.includes('www')) res.redirect(res.redirect('https://' + redirectHost + req.url));
+    else if ((req.get('X-Forwarded-Proto') !== 'https')) {
+        res.redirect('https://' + redirectHost + req.url);
+    } else next();
+})
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-}).use(express.static(path.join(__dirname,'client','build')));
-
+}).use(express.static(path.join(__dirname,'client','build')))
 
 app.listen(process.env.PORT || 4200);
 console.log(`Listening on ${process.env.PORT || 4200}`)
