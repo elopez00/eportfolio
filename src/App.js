d4919f809c42cc69f03a315af25fe7df40081bd3
Eddie Lopez <ejlopez00@gmail.com>
2021-04-21 23:04:53 -0400

    fixed name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import styled from 'styled-components'
+
 import HeyThere from './components/HeyThere'
 import AboutMe from './components/AboutMe'
-import './assets/App.css'
 import GlobalStyle from './components/GlobalStyle'
 import Projects from './components/Projects/Projects';
 import ContactMe from './components/ContactMe'
+
+import './assets/App.css'
 
 class App extends React.Component {
   state = {
