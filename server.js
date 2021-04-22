1e3a28ab53c2b702a4dc036d56d5cf4a83a1c525
Eddie Lopez <ejlopez00@gmail.com>
2021-04-22 11:26:03 -0400

    Added middleware to secure safe connection

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use((req,res,next) => {
+    const host = req.get('host');
+    let redirectHost = host.includes('www') ? host : `www.${host}`;
+
+    console.log(redirectHost)
+
+    if (host.includes('localhost')) next();
+    else if (!host.includes('www')) res.redirect(res.redirect('https://' + redirectHost + req.url));
+    else if ((req.get('X-Forwarded-Proto') !== 'https')) {
+        res.redirect('https://' + redirectHost + req.url);
+    } else next();
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+}).use(express.static(path.join(__dirname, 'client', 'build')));
+
+
+app.listen(process.env.PORT || 4200);
+console.log(`Listening on ${process.env.PORT || 4200}`)
