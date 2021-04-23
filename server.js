40df9b5f680396ede4b98631c3a9d6231eb53081
Eddie Lopez <ejlopez00@gmail.com>
2021-04-23 13:04:26 -0400

    implemented https middleware

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
+app.get('*', (req, res, next) => {
+    const host = req.headers.host;
+    if (host.includes('localhost') || host.includes('eportfolio'))
+        next();
+
+    res.redirect('https://' + host + req.url);
+})
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 })
 
-app.listen(process.env.PORT || 5000);
-console.log(`listening on ${process.env.PORT || 5000}`);
+app.listen(5000);
+console.log(`listening on ${5000}`);
