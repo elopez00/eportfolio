ead8ddefafda82007e361d7895b2efbe37fa8e73
Eddie Lopez <ejlopez00@gmail.com>
2021-04-24 08:37:03 -0400

    changing to static buildpack

---
+++ build/precache-manifest.1123acc1f997428ae973920f1c2ddac5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f68e9a2afcb2a95cab829faba501f53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7f4ab7a9f3b1d2b329c",
+    "url": "/static/css/main.eb979f67.chunk.css"
+  },
+  {
+    "revision": "8c8524bee97c26555e10",
+    "url": "/static/js/2.279cfc86.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.279cfc86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7f4ab7a9f3b1d2b329c",
+    "url": "/static/js/main.95889d01.chunk.js"
+  },
+  {
+    "revision": "fadd5b171f55da576d3a",
+    "url": "/static/js/runtime-main.c9b7527f.js"
+  },
+  {
+    "revision": "41313b452a2d07e2127d5ed476b37f43",
+    "url": "/static/media/assurancemezz.41313b45.png"
+  },
+  {
+    "revision": "40eb87453ad80cbeb0ff90d3ec8e98ae",
+    "url": "/static/media/bosawas-ui.40eb8745.jpeg"
+  },
+  {
+    "revision": "fb19b6f00d240f6a25ef0c506f4c7930",
+    "url": "/static/media/hercules.fb19b6f0.jpeg"
+  },
+  {
+    "revision": "ebe547b81e4a3584f6f4a782abd743bd",
+    "url": "/static/media/important.ebe547b8.jpeg"
+  },
+  {
+    "revision": "e2fdfec94fa1acfac48bf71af3172a1b",
+    "url": "/static/media/pageshield.e2fdfec9.jpeg"
+  },
+  {
+    "revision": "b3873b71d829e189a45ccb66d73cb3eb",
+    "url": "/static/media/room-escape.b3873b71.jpeg"
+  }
+]);
